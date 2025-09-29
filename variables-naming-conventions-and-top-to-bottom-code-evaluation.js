ef7b8bb51f112fa73b35808b09eb309a17408095
CZavos <149090367+CZavos@users.noreply.github.com>
2025-09-29 13:08:43 -0500

    Update variables-naming-conventions-and-top-to-bottom-code-evaluation.js

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,11 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
-
-console.log(d);
+let name = "Alice";
+let itemNumber = 5;
+let cost = 20;
+let declare = name + " bought " + itemNumber + " items for $" + cost + ".";
+let itemCost = cost/itemNumber
+let iCdeclare = name + " spent $" + itemCost + " per item that she bought.";
+console.log(declare);
+console.log(iCdeclare);
